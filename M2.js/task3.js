010be7e1e8fa6486c565962e5adeb8ac71741a65
autguru <comanna385@gmail.com>
2023-10-23 18:52:32 +0200

    fixed mst t4

--- M2.js/task3.js
+++ M2.js/task3.js
@@ -10,7 +10,10 @@
 "use strict";
 
 function checkForSpam(message) {
-  return message.toLowerCase().includes("spam" && "sale");
+  return (
+    message.toLowerCase().includes("spam") ||
+    message.toLowerCase().includes("sale")
+  );
 }
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
